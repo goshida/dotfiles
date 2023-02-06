74dfcac4bcaef80c202e2d8c6eb9b14c5aec4a4d
goshida <107573293+goshida@users.noreply.github.com>
2023-02-06 09:34:09 +0900

    Add static ip settings to server

--- scripts/setup-server.sh
+++ scripts/setup-server.sh
@@ -13,3 +13,13 @@ sudo cat ${_DOTFILES_ROOT}/etc/ssh/sshd_config > /etc/ssh/sshd_config
 # additional-settings
 sudo localectl --no-convert set-x11-keymap us pc104 "" ctrl:nocaps
 
+# static ip
+sudo nmcli connection add con-name wired-static type ethernet
+sudo nmcli connection modify wired-static ipv4.addresses 192.168.2.15/24
+sudo nmcli connection modify wired-static ipv4.gateway 192.168.2.1
+sudo nmcli connection modify wired-static ipv4.dns 8.8.8.8
+sudo nmcli connection modify wired-static +ipv4.dns 192.168.2.1
+sudo nmcli connection modify wired-static ipv4.method manual
+sudo nmcli connection modify wired-static ifname eno1
+sudo nmcli connection up wired-static
+
