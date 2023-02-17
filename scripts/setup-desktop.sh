08a5acd6f1242bc692ecb69d34dc2624b046468b
goshida <107573293+goshida@users.noreply.github.com>
2023-02-18 08:40:05 +0900

    Add sudo configuration

--- scripts/setup-desktop.sh
+++ scripts/setup-desktop.sh
@@ -4,13 +4,19 @@ set -eux
 
 _DOTFILES_ROOT=$( cd $( dirname $0 )/../ && pwd )
 
-# bluetooth
-sudo systemctl enable bluetooth
+_SETUP_TIMESTAMP=`date +%Y%m%d-%H%M%S`
+
+# sudo
+sudo cp -p /etc/sudoers /etc/sudoers.${_SETUP_TIMESTAMP}
+sudo sh -c "cat ${_DOTFILES_ROOT}/etc/sudoers | EDITOR=tee visudo"
 
 # gui
 sudo systemctl enable lightdm.service
-cp -pi /etc/X11/xinit/xinitrc /home/${USER}/.xinitrc
+cp --backup=simple /etc/X11/xinit/xinitrc /home/${USER}/.xinitrc
 libinput-gestures-setup autostart
+
+# bluetooth
+sudo systemctl enable bluetooth
 
 # docker
 sudo systemctl enable docker
--- scripts/setup-server.sh
+++ scripts/setup-server.sh
@@ -9,14 +9,9 @@ _SETUP_TIMESTAMP=`date +%Y%m%d-%H%M%S`
 _PUBLIC_IF="eno1"
 _WIREGUARD_IF="wg0"
 
-# sshd
-sudo cp --backup=simple -p /etc/ssh/sshd_config /etc/ssh/sshd_config.${_SETUP_TIMESTAMP}
-sudo install -o root -g root -m 644 -D ${_DOTFILES_ROOT}/etc/ssh/sshd_config /etc/ssh/sshd_config
-sudo systemctl enable sshd.service
-curl https://github.com/goshida.keys >> ~/.ssh/authorized_keys
-
-# additional-settings
-sudo localectl --no-convert set-x11-keymap us pc104 "" ctrl:nocaps
+# sudo
+sudo cp -p /etc/sudoers /etc/sudoers.${_SETUP_TIMESTAMP}
+sudo sh -c "cat ${_DOTFILES_ROOT}/etc/sudoers | EDITOR=tee visudo"
 
 # static ip
 sudo nmcli connection add con-name wired-static type ethernet
@@ -33,6 +28,12 @@ sudo cp -p /etc/nftables.conf /etc/nftables.conf.${_SETUP_TIMESTAMP}
 sudo install -o root -g root -m 644 -D ${_DOTFILES_ROOT}/etc/nftables.conf /etc/nftables.conf
 sudo systemctl enable nftables
 
+# sshd
+sudo cp --backup=simple -p /etc/ssh/sshd_config /etc/ssh/sshd_config.${_SETUP_TIMESTAMP}
+sudo install -o root -g root -m 644 -D ${_DOTFILES_ROOT}/etc/ssh/sshd_config /etc/ssh/sshd_config
+sudo systemctl enable sshd.service
+curl https://github.com/goshida.keys >> ~/.ssh/authorized_keys
+
 # WireGuard
 sudo nmcli connection add type wireguard con-name wireguard-server ifname ${WIREGUARD_IF} autoconnect no
 sudo nmcli connection modify wireguard-server ipv4.method manual ipv4.address 10.0.0.1/24
@@ -45,4 +46,7 @@ sudo install -o root -g root -m 744 -D ${_DOTFILES_ROOT}/etc/NetworkManager/disp
 sudo sed -i -e "s/public-if-dummy/${_PUBLIC_IF}/" /etc/NetworkManager/dispatcher.d/wireguard-forward.sh
 sudo sed -i -e "s/wireguard-if-dummy/${_WIREGUARD_IF}/" /etc/NetworkManager/dispatcher.d/wireguard-forward.sh
 sudo systemctl enable NetworkManager-dispatcher.service
+
+# additional-settings
+sudo localectl --no-convert set-x11-keymap us pc104 "" ctrl:nocaps
 
